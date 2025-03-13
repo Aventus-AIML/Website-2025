7c064b81e2b0f6dac317e64e774aa57d55fb4042
chetanr25 <1ds22ai010@dsce.edu.com>
2025-03-14 01:40:33 +0530

    Decreased AOS animation a bit

--- src/App.js
+++ src/App.js
@@ -1,119 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import FAQ from './Components/FAQ';
-import Footer from './Components/Footer';
-import Landing from './Components/Landing';
-import Nav from './Components/Nav';
-import Sponsors from './Components/Sponsors';
-import Tracks from './Components/Tracks';
-import About from './Components/About';
-import Prizes from './Components/Prizes';
+import React from "react";
+import Landing from "./Components/Landing";
+import Nav from "./Components/Nav";
+import Sponsors from "./Components/Sponsors";
+import Tracks from "./Components/Tracks";
+import About from "./Components/About";
 import Glimpse from "./Components/Glimpse";
 import image1 from "./images/1.png";
 import image2 from "./images/2.png";
 import image3 from "./images/3.png";
 import image4 from "./images/4.png";
 import image5 from "./images/5.png";
-import './App.css';
-import Mentors from './Components/Mentors.js';
-import Judges from './Components/judges.js';
-import Timeline from './Components/Timeline.js';
-import ProblemStatement from './Components/Problemstate.js';
-import Map from './Components/map.js';
-import { swarmBackground } from 'threejs-toys'
-import CommunityColab from './Components/CommunityColab.js';
-import IndividualSponsor from './Components/Individualsponsors.js';
-import GrandJury from './Components/GrandJury.js';
-import PrizePool from './Components/PrizePool';
-import PrevSponsors from './Components/PrevSponsors';
-import Faqs from './Components/Faqs';
-import ContactUs from './Components/ContactUs';
-import Venue from './Components/Venue';
+import "./App.css";
+import Map from "./Components/map.js";
+import IndividualSponsor from "./Components/Individualsponsors.js";
+import PrizePool from "./Components/PrizePool";
+import Faqs from "./Components/Faqs";
+import ContactUs from "./Components/ContactUs";
 
 function App() {
-    const images = [
-        image1,
-        image2,
-        image3,
-        image4,
-        image5
-    ];
+  const images = [image1, image2, image3, image4, image5];
 
-    return (
-        <>
-            <Nav />
-            <div className="conta">
-                <div>
-                    <Landing />
-                </div>
-                <div>
-                    <PrizePool />
-                </div>
-                <div>
-                    <About />
-                </div>
-                <div>
-                    <Tracks />
-                </div>
-                <div>
-                    <Sponsors />
-                </div>
-                <div>
-                    <Faqs />
-                </div>
-                {/* <div>
+  return (
+    <>
+      <Nav />
+      <div className="conta">
+        <div>
+          <Landing />
+        </div>
+        <div>
+          <PrizePool />
+        </div>
+        <div>
+          <About />
+        </div>
+        <div>
+          <Tracks />
+        </div>
+        <div>
+          <Sponsors />
+        </div>
+        <div
+          className="individual"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        ></div>
+        <div className="individual">
+          <h1>Individual Sponsors</h1>
+        </div>
+        <div style={{ padding: "20px" }}>
+          <IndividualSponsor images={images} />
+        </div>
+
+        {/* <div>
                     <CommunityColab />
                 // </div> */}
-                </div>
-                <div className="glimpse"
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                </div>
-                <div className='glimpse'>
-                    <h1>Winners of Aventus 2.0</h1>
-                </div>
-                <div style={{ padding: "20px" }}>
-                    <Glimpse images={images} />
-                </div>
-                <div className="individual"
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                </div>
-                <div className="individual">
-                    <h1>Individual Sponsors</h1>
-                </div>
-                <div style={{ padding: "20px" }}>
-                    <IndividualSponsor images={images} />
-                </div>
-                <div className="GrandJury"
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                </div>
-                <div className="contact-map-container">
-                    <div className="contact-section">
-                        <ContactUs />
-                    </div>
-                    <div className="map">
-                        <Map />
-                    </div>
-                </div>
-                {/* <div>
+
+        <div
+          className="glimpse"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        ></div>
+        <div className="glimpse">
+          <h1>Winners of Aventus 2.0</h1>
+        </div>
+        <div style={{ padding: "20px" }}>
+          <Glimpse images={images} />
+        </div>
+
+        <div>
+          <Faqs />
+        </div>
+        <div
+          className="GrandJury"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        ></div>
+        <div className="contact-map-container">
+          <div className="contact-section">
+            <ContactUs />
+          </div>
+          <div className="map">
+            <Map />
+          </div>
+        </div>
+        {/* <div>
                     <Footer />
                 </div> */}
-            </div>  
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './css/contact.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-  faInstagram, 
-  faLinkedin, 
-  faDiscord, 
-  faTelegram 
-} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "./css/contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faDiscord,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const ContactUs = () => {
   return (
     <div className="contact-section">
       <div className="contact-content">
-        <h2>Contact Us</h2>
+        <h2 style={{ color: "#2ecc71" }}>Contact Us</h2>
         <div className="contact-info">
           <div className="contact-item">
             <i className="fas fa-envelope"></i>
-            <a href="mailto:one@hackaventus.com" className="contact-link">one@hackaventus.com</a>
+            <a href="mailto:one@hackaventus.com" className="contact-link">
+              one@hackaventus.com
+            </a>
           </div>
           <div className="contact-item">
             <i className="fas fa-map-marker-alt"></i>
@@ -24,16 +26,36 @@ const ContactUs = () => {
           </div>
         </div>
         <div className="social-links">
-          <a href="https://www.instagram.com/hackaventus/" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a
+            href="https://www.instagram.com/hackaventus/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
             <FontAwesomeIcon icon={faInstagram} />
           </a>
-          <a href="https://www.linkedin.com/showcase/aventus-dsce/" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a
+            href="https://www.linkedin.com/showcase/aventus-dsce/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a href="https://discord.com/invite/Mbb9bkdsCj" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a
+            href="https://discord.com/invite/Mbb9bkdsCj"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
             <FontAwesomeIcon icon={faDiscord} />
           </a>
-          <a href="https://t.me/+SebssWzmQMVmZGI1" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a
+            href="https://t.me/+SebssWzmQMVmZGI1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
             <FontAwesomeIcon icon={faTelegram} />
           </a>
         </div>
@@ -42,4 +64,4 @@ const ContactUs = () => {
   );
 };
 
-export default ContactUs; 
+export default ContactUs;
